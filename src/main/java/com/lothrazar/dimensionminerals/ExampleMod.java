521ec63cc82943c84652cd287ae59e049e8f3c38
Sam <lothrazar@hotmail.com>
2018-10-17 18:58:24 -0700

    minerals: init empty mod

---
+++ src/main/java/com/lothrazar/dimensionminerals/ExampleMod.java
@@ -0,0 +1,30 @@
+package com.lothrazar.dimensionminerals;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ExampleMod.MODID )
+public class ExampleMod
+{
+    public static final String MODID = "dimensionminerals";
+ 
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
